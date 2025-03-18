bde72afd834ff232bc31b96ab2ce2dfe6dab70c9
AmirHosienShokri <amirhosien.shokrii@gmail.com>
2025-03-18 19:55:53 +0330

    create page 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,14 @@
+import Title from "@/app/_components/Title";
+import Link from "next/link";
+
 function NotFound() {
-  return <div></div>;
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <h3 className="text-[100px] md:text-[200px] dark:text-gray-50">404</h3>
+      <Title>صفحه ای به دنبال آن هستید پیدا نشد!</Title>
+      <Link href="/" className="bg-color-success-200 hover:bg-color-success-100 rounded-md px-4 py-2 transition-all duration-300 text-gray-50 text-lg mt-6">بازگشت به صفحه اصلی</Link>
+    </div>
+  );
 }
 
 export default NotFound;
