08922aefbb0c79284de23aa9541e654d5994e5da
AmirHosienShokri <amirhosien.shokrii@gmail.com>
2025-03-23 13:00:14 +0330

    edit page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,16 +1,13 @@
 import SelectShop from "@/app/_components/SelectShop";
-import { Suspense } from "react";
+
 import ShowShop from "@/app/_components/ShowShop";
-import Loader from "@/app/_components/Loader";
 
 export const relative = 0;
 
 async function Page() {
   return (
     <SelectShop>
-      <Suspense fallback={<Loader />}>
-        <ShowShop />
-      </Suspense>
+      <ShowShop />
     </SelectShop>
   );
 }
