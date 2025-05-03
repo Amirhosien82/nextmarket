ad27ec0a473feed63c3a6f4e54f73c7b9e220b96
AmirHosienShokri <amirhosien.shokrii@gmail.com>
2025-05-03 17:35:40 +0330

    edit json

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import CarouselShop from "@/app/_components/CarouselShop";
 import CarouselCard from "@/app/_components/CarouselCard";
 import Category from "@/app/_components/Category";
 import { getProducts } from "@/app/_lib/productService";
+import { Suspense } from "react";
+import Loader from "@/app/_components/Loader";
 
 async function Page() {
   const [specialProducts, newProducts, bestSellerProducts] = await Promise.all([
@@ -14,22 +16,28 @@ async function Page() {
   return (
     <div className="flex flex-col gap-10">
       <Slider />
-      <CarouselShop
-        title="پیشنهادات ویژه"
-        products={specialProducts.products}
-        search={"/shop?special_products=1"}
-      />
-      <CarouselShop
-        title="جدید ترین محصولات"
-        products={newProducts.products}
-        search={"/shop?orderBy=0"}
-      />
+      <Suspense fallback={<Loader />}>
+        <CarouselShop
+          title="پیشنهادات ویژه"
+          products={specialProducts.products}
+          search={"/shop?special_products=1"}
+        />
+      </Suspense>
+      <Suspense fallback={<Loader />}>
+        <CarouselShop
+          title="جدید ترین محصولات"
+          products={newProducts.products}
+          search={"/shop?orderBy=0"}
+        />
+      </Suspense>
       <Category />
-      <CarouselShop
-        title="پر فروش ترین محصولات"
-        products={bestSellerProducts.products}
-        search={"/shop?orderBy=1"}
-      />
+      <Suspense fallback={<Loader />}>
+        <CarouselShop
+          title="پر فروش ترین محصولات"
+          products={bestSellerProducts.products}
+          search={"/shop?orderBy=1"}
+        />
+      </Suspense>
       <CarouselCard title="مطالب خواندنی" />
     </div>
   );
